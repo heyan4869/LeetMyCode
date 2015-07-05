c96fd048ca4a7aa462277acfb169de0270a34317
Yan <heyan4869@gmail.com>
2015-07-05 12:15:20 +0800

    Daily update

--- src/bit/NumberOf1Bits.java
+++ src/bit/NumberOf1Bits.java
@@ -3,12 +3,19 @@ package bit;
 public class NumberOf1Bits {
 	// Need to treat n as an unsigned value
 	public static int hammingWeight(int n) {
-
+		int count = 0;
+		for (int i = 0; i < 32; i++) {
+			if ((n & 1) == 1) {
+				count++;
+			}
+			n = n >> 1;
+		}
+		return count;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(hammingWeight(11));
 	}
 
 }
